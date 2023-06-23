e7f2df023062ef7e7d9d313182486e2f35aed7ca
matheusjasbick <mjasbick@gmail.com>
2023-06-23 15:39:48 -0300

    final adjustments

--- script.js
+++ script.js
@@ -8,13 +8,8 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     //se tiver light mode, add a img light
     img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt', 'Foto de Matheus Jasbick abraçado com  sua namorada')
   } else {
   //se tiver sem light mode, manter a img normal
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Matheus Jasbick, usando roupa preta com um casaco azul amarrado nas costas"
-    )
   }
 }
